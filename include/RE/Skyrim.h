299398682d25673ce4793d1e777807824805050b
kkEngine <78795688+kkEngine@users.noreply.github.com>
2024-08-28 12:47:37 +0300

    @epinter map RE sync

--- include/RE/Skyrim.h
+++ include/RE/Skyrim.h
@@ -1288,6 +1288,7 @@
 #include "RE/M/MapInputHandler.h"
 #include "RE/M/MapLookHandler.h"
 #include "RE/M/MapMenu.h"
+#include "RE/M/MapMenuMarker.h"
 #include "RE/M/MapMoveHandler.h"
 #include "RE/M/MapZoomHandler.h"
 #include "RE/M/MarkerUsedData.h"
